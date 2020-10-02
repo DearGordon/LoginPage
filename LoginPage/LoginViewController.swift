d214de6a8d0272ed6d7d2877b1f256c11999cf0c
GordonFeng <hellogordon.f@gmail.com>
2020-10-02 17:10:28 +0800

    commit

---
+++ LoginPage/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Gordon Feng on 2/10/20.
+//  Copyright © 2020 GordonFeng. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
